f99209f590cd636b436b4b3814bdf148aa6de37e
Ange Chierchia <ange.chierchia@gmail.com>
2018-02-25 16:55:58 +0100

    Prend en compte l'anglais pour `emailSubject`

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function () {
     var emailLink = document.getElementById('send-email');
     var emailName = "aihcreihc.egna";
     var emailTLD = "moc.liamg";
-    var emailSubject = "Votre profil m'intéresse";
+    var emailSubject = document.querySelector("html").getAttribute('lang') === "fr" ? "Votre profil m'intéresse" : "I'm interested in your profile";
 
     var email = emailName.split("").reverse().join("") + "@";
     email += emailTLD.split("").reverse().join("");
@@ -12,39 +12,3 @@ document.addEventListener('DOMContentLoaded', function () {
     emailLink.setAttribute("href", "mailto:" + email + "?subject=" + encodeURIComponent(emailSubject));
     emailLink.innerHTML = email;
 });
-
-fetch('../../public/jobs.json').then(function (response) {
-    return response.json();
-}).then(function (data) {
-    var jobsList = [];
-    data.map(function (job) {
-        var jobItem = "<li class='list__item fetched'>";
-        jobItem += "<div class='card'>";
-        jobItem += "<h3 class='card__title'>" + job.title + "</h3>";
-        jobItem += "<div class='card__meta'><span class='date--start'>" + job.start + "</span>";
-        jobItem += "<span class='date--end'>" + job.end + "</span></div>";
-        jobItem += "<div class='card__description'><div class='company'>";
-        jobItem += job.url ? "<a href='" + job.url + "' class='company__name'>" + job.company + "</a>, " : "<span class='company__name'>" + job.company + "</span>, ";
-        jobItem += "<span class='company__location'>" + job.location + "</span></div>" + job.description;
-        jobItem += "</div></div></li>";
-        jobsList.push(jobItem);
-        console.log(job);
-    });
-    document.querySelector('#jobs ol.list').innerHTML = jobsList.join("");
-});
-
-/*
-<li class="list__item">
-    <div class="card">
-        <h3 class="card__title">D&eacute;veloppeur Web</h3>
-        <div class="card__meta">
-            <span class="date--start">janv. 2015</span><span class="date--end">En poste</span>
-        </div>
-        <div class="card__description">
-            <div class="company">
-                <a href="https://www.ibakus.com" class="company__name">Ibakus Europe SA</a>, <span class="company__location">Strassen</span>
-            </div>
-        </div>
-    </div>
-</li>
-*/
