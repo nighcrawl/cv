01b64f72eeae50721a86c1b8e51292d040db817d
Ange Chierchia <achierchia@conceptfactory.lu>
2021-08-04 16:05:14 +0200

    relative time

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,35 @@
 'use strict';
 
+function relativeTime(start, end, lang = 'fr') {
+    if (typeof(end) === undefined) {
+        end = new Date();
+    } else {
+        end = new Date(Date.parse(end));
+    }
+    start = new Date(Date.parse(start));
+
+    var labelYear = lang === 'fr' ? 'an' : 'year';
+    var labelMonth = lang === 'fr' ? 'mois' : 'month';
+    var labelAnd = lang === 'fr' ? 'et' : 'and';
+
+    var distance  = end.getTime() - start.getTime();
+    var years = Math.floor(distance / (24 * 60 * 60 * 1000 * 365));
+    var months = Math.floor(distance / (24 * 60 * 60 * 1000 * 365/12));
+
+    var string = '';
+
+    if (years > 0) {
+        string += years + ' ' + labelYear + (years > 1 ? 's' : '');
+        if (months > 0) {
+            string += ' ' + labelAnd + ' ' + months + ' ' + labelMonth + (months > 1 && lang !== 'fr' ? 's' : '');
+        }
+    } else {
+        string += months + ' ' + labelMonth + (months > 1 && lang !== 'fr' ? 's' : '');
+    }
+
+    return string;
+}
+
 document.addEventListener('DOMContentLoaded', function () {
     var emailLink = document.getElementById('send-email');
     var emailName = "boj+aihcreihc.egna";
@@ -11,4 +41,15 @@ document.addEventListener('DOMContentLoaded', function () {
 
     emailLink.setAttribute("href", "mailto:" + email + "?subject=" + encodeURIComponent(emailSubject));
     emailLink.innerHTML = email;
+
+    var lang = document.querySelector('html').getAttribute('lang');
+
+    document.querySelectorAll('.relative-time').forEach(function(el, idx) {
+        var start = el.getAttribute('data-start');
+        var end = el.getAttribute('data-end');
+
+        var relative = relativeTime(start, end, lang);
+
+        el.innerHTML = relative;
+    });
 });
