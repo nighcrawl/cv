f632f09e3762cb8b04f8ba194372c5952319435a
Ange Chierchia <achierchia@conceptfactory.lu>
2025-08-15 13:45:09 +0200

    fix(ci): add release into commitlint accepted scopes

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
   // Optionnel : verrouille quelques scopes utilisés dans ton repo
   rules: {
     'scope-enum': [2, 'always', [
-      'templates','styles','i18n','pdf','og','pages','ci','build','data','scripts','repo'
+      'templates','styles','i18n','pdf','og','pages','ci','build','data','scripts','repo','release'
     ]]
   }
 };
