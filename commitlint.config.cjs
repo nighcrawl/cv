4b564204eb15336ba71c2bbc27cc7a86e8119476
Ange Chierchia <achierchia@conceptfactory.lu>
2025-08-15 13:58:08 +0200

    ci(release): shorten semantic-release commit and relax commitlint
    - semantic-release: set git commit message to "chore(release): ${nextRelease.version} [skip ci]"
    - commitlint: ignore release commits; disable body/footer line-length checks
    - commitlint: allow 'release' scope

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,7 +1,9 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  // Optionnel : verrouille quelques scopes utilisés dans ton repo
+  ignores: [(msg) => /^chore\(release\): /.test(msg)],
   rules: {
+    'body-max-line-length': [0],
+    'footer-max-line-length': [0],
     'scope-enum': [2, 'always', [
       'templates','styles','i18n','pdf','og','pages','ci','build','data','scripts','repo','release'
     ]]
